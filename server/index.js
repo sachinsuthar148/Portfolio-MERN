cae73a6905921de41fd410698b8d6be201dc9e47
2021pietcssachin148@poornima.org <2021pietcssachin148@poornima.org>
2024-05-05 11:05:41 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app= express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+mongoose.connect('mongodb+srv://mistrysachin185:Mongo%40123@cluster0.bwkyxxx.mongodb.net/Portfolio', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+.then(()=>{console.log("Connected to database")})
+.catch((error) => handleError(error));
+
+// Or:
+
+let dataSchema = mongoose.Schema(
+    {
+    name:{
+        type:String,
+        require: true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    message:{
+        type:String,
+        require:true
+    }
+}   
+)
+
+let PortfolioData = mongoose.model("portfoliodata",dataSchema);
+
+//Home API
+app.post("/",async (req,res)=>{  
+    try{
+        let {name,email,message} = req.body;
+        const data = {
+            name:name,
+            email:email,
+            message:message
+        }
+        await PortfolioData.insertMany([data]);
+        res.status(201).json({ message: "Data saved successfully" });
+      }catch(err){
+        console.log(err);
+        res.status(500).json({ message: "Internal Server Error" });
+      }
+    
+
+    
+});
+
+
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+})
